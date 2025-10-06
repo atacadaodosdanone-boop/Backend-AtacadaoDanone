23e4f8021449c1479b8ac238029745d65235ac89
atacadaodosdanone-boop <236001100+atacadaodosdanone-boop@users.noreply.github.com>
2025-10-06 01:01:44 -0400

    Version 8: Save current changes

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ class Config:
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
     
     # MongoDB Configuration
-    MONGO_URI = os.environ.get(\'MONGO_URI\') or \'mongodb+srv://atacadaodosdanone_db_user:PFZdVRSnsBm1Q5ZY@cluster0.lplaiox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0\' 
+    MONGO_URI = os.environ.get('MONGO_URI') or 'mongodb+srv://atacadaodosdanone_db_user:PFZdVRSnsBm1Q5ZY@cluster0.lplaiox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' 
     # CORS Configuration
     CORS_ORIGINS = ['*']  # Permitir qualquer origem em produção
 
